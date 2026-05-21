5da4ec92ad5eb70b1847c3d7a427dd74a252660c
Cri-Bon <bonomi.cr08@gmail.com>
2026-05-21 20:30:40 +0200

    Update script.js
    fix

--- script.js
+++ script.js
@@ -2,37 +2,26 @@ const form = document.getElementById("contactForm");
 const successMessage = document.getElementById("successMessage");
 
 form.addEventListener("submit", function(event){
-
   event.preventDefault();
-
   const name = document.getElementById("name").value;
-
   successMessage.innerHTML =
     "Grazie " + name + "! La tua richiesta è stata inviata con successo.";
-
   form.reset();
-  // FAQ ACCORDION
+});
+
 const faqItems = document.querySelectorAll(".faq-item");
 
 faqItems.forEach(item => {
-
   const question = item.querySelector(".faq-question");
 
   question.addEventListener("click", () => {
-
     // chiude tutte le altre FAQ
     faqItems.forEach(faq => {
-
       if(faq !== item){
         faq.classList.remove("active");
       }
-
     });
-
     // apre/chiude quella cliccata
     item.classList.toggle("active");
-
   });
-
-});
 });
