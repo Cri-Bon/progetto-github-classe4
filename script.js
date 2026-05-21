cb968b70911ac3c58c8f7ee2ff077c5ffce5c6b6
Elia Camanini <eliacamanini@gmail.com>
2026-05-21 11:29:58 +0200

    Update faq script.js

--- script.js
+++ script.js
@@ -11,4 +11,28 @@ form.addEventListener("submit", function(event){
     "Grazie " + name + "! La tua richiesta è stata inviata con successo.";
 
   form.reset();
+  // FAQ ACCORDION
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", () => {
+
+    // chiude tutte le altre FAQ
+    faqItems.forEach(faq => {
+
+      if(faq !== item){
+        faq.classList.remove("active");
+      }
+
+    });
+
+    // apre/chiude quella cliccata
+    item.classList.toggle("active");
+
+  });
+
+});
 });
