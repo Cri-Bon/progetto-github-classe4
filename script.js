a23dd8e4884ab22408caaf373deb3010e6c88703
Cri-Bon <bonomi.cr08@gmail.com>
2026-05-22 08:33:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
 const form = document.getElementById("contactForm");
 const successMessage = document.getElementById("successMessage");
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const name = document.getElementById("name").value;
-  successMessage.innerHTML =
-    "Grazie " + name + "! La tua richiesta è stata inviata con successo.";
-  form.reset();
+
+  // input
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const service = document.getElementById("service");
+  const message = document.getElementById("message");
+
+  // reset messaggi
+  successMessage.innerHTML = "";
+  document.querySelectorAll(".error").forEach(el => el.remove());
+
+  let isValid = true;
+
+  // VALIDAZIONE NOME
+  if (name.value.trim() === "") {
+    showError(name, "Inserisci il tuo nome");
+    isValid = false;
+  }
+
+  // VALIDAZIONE EMAIL
+  if (email.value.trim() === "") {
+    showError(email, "Inserisci la tua email");
+    isValid = false;
+  }
+
+  // VALIDAZIONE SERVIZIO
+  if (service.value === "") {
+    showError(service, "Seleziona un servizio");
+    isValid = false;
+  }
+
+  // VALIDAZIONE MESSAGGIO
+  if (message.value.trim() === "") {
+    showError(message, "Scrivi un messaggio");
+    isValid = false;
+  }
+
+  // SUCCESSO
+  if (isValid) {
+    successMessage.innerHTML =
+      "Grazie " + name.value + "! La tua richiesta è stata inviata con successo.";
+
+    successMessage.style.color = "green";
+
+    form.reset();
+  }
 });
 
-const faqItems = document.querySelectorAll(".faq-item");
+// funzione errore
+function showError(input, message) {
+  const error = document.createElement("p");
+  error.className = "error";
+  error.innerText = message;
 
-faqItems.forEach(item => {
-  const question = item.querySelector(".faq-question");
+  error.style.color = "red";
+  error.style.fontSize = "14px";
+  error.style.marginTop = "-15px";
+  error.style.marginBottom = "15px";
 
-  question.addEventListener("click", () => {
-    // chiude tutte le altre FAQ
-    faqItems.forEach(faq => {
-      if(faq !== item){
-        faq.classList.remove("active");
-      }
-    });
-    // apre/chiude quella cliccata
-    item.classList.toggle("active");
-  });
-});
+  input.insertAdjacentElement("afterend", error);
+}
